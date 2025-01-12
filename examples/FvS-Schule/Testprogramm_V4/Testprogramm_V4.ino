4afd9c17f162f22f6e62d0893dc1a18b76a9a3b0
Constantin Tress <constantin.tress@t-online.de>
2025-01-12 08:32:08 +0100

    Kommentare hinzugefügt

--- examples/FvS-Schule/Testprogramm_V4/Testprogramm_V4.ino
+++ examples/FvS-Schule/Testprogramm_V4/Testprogramm_V4.ino
@@ -1,5 +1,7 @@
+// einbinden der FVS Bibliothek
 #include <FVS.h>
 
+// Definition der Pins zu den LEDs -> Port1
 #define LED0  1
 #define LED1  2
 #define LED2  3
@@ -8,6 +10,8 @@
 #define LED5  14
 #define LED6  15
 #define LED7  16
+
+// Definition der Pins zu den Eingängen -> Port0
 #define S0  25
 #define S1  26
 #define S2  27
@@ -20,12 +24,16 @@
 
 
 Generic_LM75 Temperature;
+// Array für Ein/-Ausgänge
 unsigned char leds[8]={LED0,LED1,LED2,LED3,LED4,LED5,LED6,LED7};
 unsigned char schalter[8]={S0,S1,S2,S3,S4,T0,T1,T2};
+
 
 void setup() {
   int i;
   uint16_t calData[5];
+  //initialisierung des Displays -> Tft.begin
+  //Objekt wird in FVS-Bibliothe erstellt
   Tft.init();
    Tft.setRotation(3);
   Tft.calibrateTouch(calData, TFT_MAGENTA, TFT_BLACK, 15);
@@ -34,6 +42,7 @@ void setup() {
   Tft.setTextColor(TFT_BLUE,TFT_WHITE); 
   Tft.setTextSize(2);
  
+  //Mode für Ein/-Ausgänge festlegen
   for(i=0; i<8; i++)
   { 
     pinMode(leds[i],OUTPUT);
@@ -48,6 +57,7 @@ void loop() {
   uint16_t x, y;
 
   // See if there's any touch data for us
+  //Eingabeprüfung Touch
   if (Tft.getTouch(&x, &y))
   {
     //y=239-y;
@@ -62,18 +72,21 @@ void loop() {
         }
     }
   }
-    
+  
+  //Ausgabe auf dem Display
   Tft.setTextColor(TFT_GREEN,TFT_WHITE);
   Tft.drawCentreString("Testprogramm",160,0,2);
   Tft.setCursor(0, 40, 2);
   Tft.setTextColor(TFT_BLUE,TFT_WHITE);
+  //Ausgabe Analogwert
   Tft.println("AD-Wert: " + (String)analogRead(POTI)+"   ");
   Tft.println("Spannung: " + (String)(analogRead(POTI)*3.3/4095) + "V  ");
-  
+  //Ausgabe Zustände der Schalter
   for(i=0; i<8; i++)
   { 
    digitalWrite(leds[i],digitalRead(schalter[i]));
   }
+  //Ausgabe Temperatur
   Tft.setTextColor(TFT_RED,TFT_WHITE); 
   Tft.print("Temperatur: ");
   Tft.print(Temperature.readTemperatureC());
