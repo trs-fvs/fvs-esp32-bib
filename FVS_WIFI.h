4bbf307c847887e22072eff9bac8bd010ed7ae32
Constantin Tress <constantin.tress@t-online.de>
2025-02-28 18:15:44 +0100

    Hinzufügen von wfifBegin Methode
    und optische anpassung

--- FVS_WIFI.h
+++ FVS_WIFI.h
@@ -1,29 +1,29 @@
 /***************************************************************************
- * Ferdinand von Steinbeis-Schule Reutlingen
- * 
- * W-LAN und mqtt Klassen und Definitionen
- * 
- * Autor: Tress
- * Datum: 20.02.2025
- * 
- * Bearbeitet: Tress
- * Datum:
- * 
- * Standartdefinition für Verbindung in der Schule:
- * W-Lan SSID: FVS-MPSK
- * Password: iotatfvs
- * mqtt-Server: 10.190.2.13 oder mqtt01.pn.steinbeis.schule, nur innerhalb des Schulnetztes erreichbar
- * mqtt-Port: 1883
- * 
+* Ferdinand von Steinbeis-Schule Reutlingen
+*
+* W-LAN und mqtt Klassen und Definitionen
+*
+* Autor: Tress
+* Datum: 20.02.2025
+*
+* Bearbeitet: Tress
+* Datum:
+*
+* Standartdefinition für Verbindung in der Schule:
+* W-Lan SSID: FVS-MPSK
+* Password: iotatfvs
+* mqtt-Server: 10.190.2.13 oder mqtt01.pn.steinbeis.schule, nur innerhalb des Schulnetztes erreichbar
+* mqtt-Port: 1883
+*
 ***************************************************************************/
 
 #ifndef _FVS_WIFI_H
 #define _FVS_WIFI_H
 
 #ifdef  _FVS_WIFI_C
-  #define WIFI_EXT
+#define WIFI_EXT
 #else
-  #define WIFI_EXT extern
+#define WIFI_EXT extern
 #endif
 
 #include <WiFi.h>
@@ -41,24 +41,27 @@
 class fvs_wifi{
 public:
 
-//Variablen
-char *fvsWifi_ssid;
-char *fvsWifi_pw;
-char *mqtt_server;
-int mqtt_port;
-char* mqtt_user;
-char* mqtt_pw;
+  //Variablen
+  char *fvsWifi_ssid;
+  char *fvsWifi_pw;
+  char *mqtt_server;
+  int mqtt_port;
+  char* mqtt_user;
+  char* mqtt_pw;
 
-//Konstruktor mit Standardwerte
-fvs_wifi(char* ssid = WIFI_SSID, char* pw = WIFI_PW, char* server = MQTT_SERVER, int port = MQTT_PORT, char* user = MQTT_USER, char* key = MQTT_KEY); //Konstruktor mit Standard initialisierung
+  //Konstruktor mit Standardwerte
+  fvs_wifi(char* ssid = WIFI_SSID, char* pw = WIFI_PW, char* server = MQTT_SERVER, int port = MQTT_PORT, char* user = MQTT_USER, char* key = MQTT_KEY); //Konstruktor mit Standard initialisierung
 
-//Methoden Prototypen für Rückgabe der Werte
-char* wlanSsid(void);
-char* wlanPw(void);
-char* mqttServer(void);
-int mqttPort(void);
-char* mqttUser(void);
-char* mqttPw(void);
+  //Methoden Prototypen für Rückgabe der Werte
+  char* wlanSsid(void);
+  char* wlanPw(void);
+  char* mqttServer(void);
+  int mqttPort(void);
+  char* mqttUser(void);
+  char* mqttPw(void);
+
+  //Methoden WiFi
+  void wifiBegin(void);
 
 private:
 
