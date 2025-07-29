0369aa0e2679e21fc127ae54a4623c548c0ea545
trs-fvs <constantin.tress@steinbeis.schule>
2025-07-29 14:26:16 +0200

    Update MAC-Adresse.ino
    neues Board, neue Methoden

--- examples/FvS-Schule/MAC-Adresse/MAC-Adresse.ino
+++ examples/FvS-Schule/MAC-Adresse/MAC-Adresse.ino
@@ -1,11 +1,16 @@
 /*********************************************************
 Anzeigen der MAC-Adressen vom Schul uController
 
-Tress Constantin 18. Feb 2025
+Tress Constantin 29. Jul 2025
 *********************************************************/
 
 #include <FVS.h>
-#include <WiFi.h>
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_log.h"
+
+//variable to store mac-adress
+uint8_t mac[6];
 
 void setup() {
   Tft.begin();
@@ -15,40 +20,30 @@ void setup() {
   portMode(1, OUTPUT);
   portWrite(1, 0);
 
-  // Variable to store the MAC address
-  uint8_t baseMac[6];
+  // Get the base MAC of the ESP chip (works always)
+  Network.macAddress(mac);
+  Tft.printf("MAC-Adresse:\n%02X:%02X:%02X:%02X:%02X:%02X\n",
+             mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
 
-  // Get MAC address of the WiFi station interface
-  esp_read_mac(baseMac, ESP_MAC_WIFI_STA);
-  Tft.println("Station MAC: ");
-  for (int i = 0; i < 5; i++) {
-    Tft.printf("%02X:", baseMac[i]);
-  }
-  Tft.printf("%02X\n", baseMac[5]);
+  /*
+  // Get the MAC address of WiFi STA (must first start interface)
+  WiFi.STA.begin();
+  WiFi.STA.macAddress(mac);
+  Tft.printf("MAC-Adresse:\n%02X:%02X:%02X:%02X:%02X:%02X\n",
+             mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
 
-  // Get the MAC address of the Wi-Fi AP interface
-  esp_read_mac(baseMac, ESP_MAC_WIFI_SOFTAP);
-  Tft.println("SoftAP MAC: ");
-  for (int i = 0; i < 5; i++) {
-    Tft.printf("%02X:", baseMac[i]);
-  }
-  Tft.printf("%02X\n", baseMac[5]);
+  // Get the MAC address of WiFi AP (must first start interface)
+  WiFi.AP.begin();
+  WiFi.AP.macAddress(mac);
+  Tft.printf("MAC-Adresse:\n%02X:%02X:%02X:%02X:%02X:%02X\n",
+             mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
 
-  // Get the MAC address of the Bluetooth interface
-  esp_read_mac(baseMac, ESP_MAC_BT);
-  Tft.println("Bluetooth MAC: ");
-  for (int i = 0; i < 5; i++) {
-    Tft.printf("%02X:", baseMac[i]);
-  }
-  Tft.printf("%02X\n", baseMac[5]);
-
-  // Get the MAC address of the Ethernet interface
-  esp_read_mac(baseMac, ESP_MAC_ETH);
-  Tft.println("Ethernet MAC: ");
-  for (int i = 0; i < 5; i++) {
-    Tft.printf("%02X:", baseMac[i]);
-  }
-  Tft.printf("%02X\n", baseMac[5]);
+  // Get the MAC address of ETH (must first start interface)
+  ETH.begin();
+  ETH.macAddress(mac);
+  Tft.printf("MAC-Adresse:\n%02X:%02X:%02X:%02X:%02X:%02X\n",
+             mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+*/
 }
 
 void loop() {
