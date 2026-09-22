6f449c932e14a326d96517ff9577f8c4079ce397
Constantin Tress <constantin.tress@t-online.de>
2026-09-22 16:54:18 +0200

    neues Beispiel für Schrittmotor und 7-Segment

---
+++ examples/FvS-Schule/littleHelper/littleHelper.ino
@@ -0,0 +1,6 @@
+//Ausgabeschema Schrittmotor:
+{ 10, 9, 5, 6 }
+
+//Bitmuster für die 7-Segmentanzeige:
+//  0     1     2     3     4     5     6     7    8     9
+{ 0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F }
